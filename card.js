38e5c91a64c46320df23c6ceaed9fc434feddcb9
julianjohannesen <github email address>
2019-11-25 16:44:21 -0500

    fixing typos and refactoring in card.js

--- card.js
+++ card.js
@@ -1,30 +1,37 @@
-export function generateCard(info) {
-    const date = new Date(info["published"])
+export function generateCard({
+    published, 
+    photos, 
+    name, 
+    url, 
+    description
+}) {
+
+    const date = new Date(published)
 	return `
     <div class="card">
     <div class="card-image">
         <figure class="image is-4by3">
-            <img src=${info["photos"][0]["full"]} alt=${info["name"]}>
+            <img src=${photos[0]["full"]} alt=${name]}>
         </figure>
     </div>
     <div class="card-content">
         <div class="media">
         <div class="media-left">
             <figure class="image is-48x48">
-            <img src=${info["photos"][1]["small"]} alt=${info["name"]}>
+            <img src=${photos[0]["small"]} alt=${name}>
             </figure>
         </div>
         <div class="media-content">
-            <p class="title is-4">${info["name"]}</p>
-            <p class="subtitle is-6"><a href=${info["url"]}  alt=${info["name"]}>Link on Petfinder</a></p>
+            <p class="title is-4">${name}</p>
+            <p class="subtitle is-6"><a href=${url}  alt=${name}>Link on Petfinder</a></p>
         </div>
         </div>
 
         <div class="content">
-        ${info["description"]}.
+        ${description}.
         
         <br>
-        <time datetime=${date.getYear()-date.getMonth()-date.getDate()}>Updated: ${date.getMonth()-date.getDate()-date.getYear()}</time>
+        <time datetime=${date.getYear()}-${date.getMonth()}-${date.getDate()}>Updated: ${date.getMonth()}-${date.getDate()}-${date.getYear()}</time>
         </div>
     </div>
     </div>
